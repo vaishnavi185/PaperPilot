fbcedf8e7e153ff38a60c0253d883fadc3388b13
vaishnavi <vishu032004@gmail.com>
2026-07-14 21:37:38 +0530

    added board crud

--- backend/routes/board_routes.py
+++ backend/routes/board_routes.py
@@ -1,6 +1,8 @@
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from dependencies.auth import get_current_user
 from database.connectivity import get_db
 from controller.BoardController import BoardController
 from schemes.board import (
@@ -15,30 +17,46 @@ board_router = APIRouter(
 )
 
 
-@board_router.post("/", response_model=BoardResponse)
-def create_board(board: BoardCreate, db: Session = Depends(get_db)):
+@board_router.post("/add-board", response_model=BoardResponse)
+def create_board(
+    board: BoardCreate,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
     return BoardController.create_board(board, db)
 
 
-@board_router.get("/", response_model=list[BoardResponse])
-def get_all_boards(db: Session = Depends(get_db)):
+@board_router.get("/get-board", response_model=list[BoardResponse])
+def get_all_boards(
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
     return BoardController.get_all_boards(db)
 
 
-@board_router.get("/{board_id}", response_model=BoardResponse)
-def get_board(board_id: int, db: Session = Depends(get_db)):
+@board_router.get("/get_by_id/{board_id}", response_model=BoardResponse)
+def get_board(
+    board_id: int,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
     return BoardController.get_board_by_id(board_id, db)
 
 
-@board_router.put("/{board_id}", response_model=BoardResponse)
+@board_router.put("/update-board/{board_id}", response_model=BoardResponse)
 def update_board(
     board_id: int,
     board: BoardUpdate,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
 ):
     return BoardController.update_board(board_id, board, db)
 
 
-@board_router.delete("/{board_id}")
-def delete_board(board_id: int, db: Session = Depends(get_db)):
+@board_router.delete("/delete-board/{board_id}")
+def delete_board(
+    board_id: int,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
     return BoardController.delete_board(board_id, db)
